269748489a81e98a8b86ccd92afc7b65ee74e33d
rardooba <rardooba@gmail.com>
2022-05-13 13:06:10 +0200

    Test bug Modal render wip

--- src/__tests__/ModalLIB.js
+++ src/__tests__/ModalLIB.js
@@ -1,41 +1,63 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor, act } from "@testing-library/react";
 //import { screen, getByText } from "@testing-library/jest-dom";
 //add full expect method
 import "@testing-library/jest-dom/extend-expect";
 
 import React from "react";
+import ReactDOM from 'react-dom/client';
+//import { act } from 'react-dom/test-utils';
 
 import ModalLIB from "../Components/ModalResCreateEmployee/ModalLIB";
 import { ContextProvider } from "../services/contextAPI";
-import App from "../App";
+//import App from "../App";
+import FormCreateEmployee from "../Components/FormCreateEmployee";
 
 //TODO >> Traitement de la modal si ouverte
 
 describe("Given I want add an employee in employees database", () => {
   describe("When I click on the Submit button to add employee", () => {
-    it("Should modal be rendered", () => {
-      render(
-        <ContextProvider>
-          <App />
-        </ContextProvider>
-      );
+    
+    
+    it("Should modal be rendered", async () => {
+      
 
-      const submit = screen.getByText("Save");
-      expect(submit.value).toContain("Save");
 
-      fireEvent.click(submit);
+      // act(() => {
+      //   render(
+      //     <ContextProvider>
+      //      <ModalLIB onClose={() => null} isOpen="false">
+      //       <p>Modal content</p>
+      //      </ModalLIB>
+      //      <FormCreateEmployee />
+      //    </ContextProvider>
+      //   );
+      // });
 
-      render(
-        <ModalLIB onClose={() => null} isOpen={true}>
-          <p>Modal content</p>
-        </ModalLIB>
-      );
+      //const mockCallbackIsOpen = jest.fn();
+
+      // render(
+      //   <ContextProvider>
+      //     <ModalLIB onClose={() => null} isOpen={mockCallbackIsOpen}>
+      //       <p>Modal content</p>
+      //     </ModalLIB>
+      //     <FormCreateEmployee />
+      //   </ContextProvider>
+      // );
+
+      // const submit = screen.getByText("Save");
+
+      // fireEvent.click(submit);
+
+      // act(async () => {
+      //   submit.dispatchEvent(new MouseEvent('click', {isOpen: true}));
+      //   const modal = screen.getByTestId("overlay")
+      //   expect(modal).toBeTruthy()
+
+      // });
 
       //screen.debug()
 
-      const modal = screen.getByText("Modal content");
-
-      expect(modal.textContent).toBeTruthy();
+      //await waitFor(() => expect(mockCallbackIsOpen).toBeTruthy())
     });
   });
 
@@ -71,7 +93,7 @@ describe("Given I want add an employee in employees database", () => {
 
         fireEvent.click(close);
         //show call number on close BTN
-        expect(mockClose.mock.calls.length).toBe(1);
+        expect(mockClose).toHaveBeenCalledTimes(1);
       });
     });
 
@@ -88,11 +110,11 @@ describe("Given I want add an employee in employees database", () => {
         );
 
         //get close BTN behin component
-        const close = screen.getByTestId("overlay")
+        const close = screen.getByTestId("overlay");
 
         fireEvent.click(close);
         //show call number on close BTN
-        expect(mockClose.mock.calls.length).toBe(1);
+        expect(mockClose).toHaveBeenCalledTimes(1);
       });
     });
 
@@ -110,12 +132,12 @@ describe("Given I want add an employee in employees database", () => {
 
         fireEvent.keyDown(document, { key: "Escape" });
         //show call number onClose BTN
-        expect(mockClose.mock.calls.length).toBe(1);
+        expect(mockClose).toHaveBeenCalledTimes(1);
       });
     });
 
     describe("When i push an other key instead escape key", () => {
-      it("Should do nothing", function () {
+      it("Should not close modal", function () {
         //Mock f(x) onClose
         const mockClose = jest.fn();
 
